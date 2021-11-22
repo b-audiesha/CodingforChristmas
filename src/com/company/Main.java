ba24c467da18c946066f488c9fb951d505ceffa4
Brattin <audiesha.brattin@accenture.com>
2021-11-22 15:14:08 -0600

    arrays excersises

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,28 @@ package com.company;
 
 import java.awt.*;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        System.out.println(Arrays.toString(numbers));
+
+
        //String message = new String("Hello World");  is the same as ==>
-        String name = "Audiesha";
-        String message = "         Hello World " + "My Name is" + " " + name;
-        System.out.println(message);
-        System.out.println(message.endsWith("Audiesha"));
-        System.out.println(message.startsWith("O"));
-        System.out.println(message.indexOf("i"));
-        System.out.println(message.replace("Audiesha","Kyle"));
-        System.out.println(message.trim());
-        System.out.println("Hello \"Audiesha\"");
-        System.out.println("Hello \nAudiesha");
+//        String name = "Audiesha";
+//        String message = "         Hello World " + "My Name is" + " " + name;
+//        System.out.println(message);
+//        System.out.println(message.endsWith("Audiesha"));
+//        System.out.println(message.startsWith("O"));
+//        System.out.println(message.indexOf("i"));
+//        System.out.println(message.replace("Audiesha","Kyle"));
+//        System.out.println(message.trim());
+//        System.out.println("Hello \"Audiesha\"");
+//        System.out.println("Hello \nAudiesha");
     }
 }
